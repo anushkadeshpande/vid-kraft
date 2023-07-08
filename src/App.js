e8ae1088c8f9668df6736fd90ff82cc83ee43848
anushkadeshpande <anushkaa5000@gmail.com>
2023-07-09 03:25:12 +0530

    feat: Load and render local video

--- src/App.js
+++ src/App.js
@@ -1,13 +1,67 @@
-import './App.css';
-const { ipcRenderer } = window.require('electron');
+import { useState, useRef, useEffect } from "react";
+import "./App.css";
+const { ipcRenderer } = window.require("electron");
 // const { remote } = window.require('electron');
 // const { dialog } = remote;
 
 function App() {
-const pressed = () => {
-  console.log("Button pressed")
-  ipcRenderer.sendSync('convert-video')
+  const pressed = () => {
+    console.log("Button pressed");
+    ipcRenderer.sendSync("convert-video");
+  };
+
+  const handleFileSelect = (event) => {
+    const file = event.target.files[0]; // Get the selected file
+
+    if (file) {
+      const filePath = URL.createObjectURL(file); // Get a temporary URL for the selected file
+      console.log("Selected file path:", filePath);
+    }
+  };
+
+  function openFile() {
+    ipcRenderer
+      .invoke("open-file-dialog")
+      .then((filePath) => {
+        console.log("Selected file path:", filePath);
+        setVidPath(filePath);
+      })
+      .catch((error) => {
+        console.error("Error while opening file dialog:", error);
+      });
+  }
+
+  const [vidPath, setVidPath] = useState("");
+  // let ctx = null
+  useEffect(() => {
+    const canvas = document.getElementById("canV");
+    let ctx = canvas.getContext("2d");
+    var video = document.createElement("video");
+    if(vidPath) {
+    video.src = vidPath;
+
+    video.play(); // start playing
+    update(); //Start rendering
+    }
+    function update() {
+      ctx.drawImage(video, 0, 0, 256, 256);
+      requestAnimationFrame(update); // wait for the browser to be ready to present another animation fram.
+    }
+  }, [vidPath]);
+  const canvasRef = useRef();
+
+  return (
+    <div className="App">
+      {/* <input type='file' onChange={handleFileSelect}/> */}
+      {/* <h1>VidKraft</h1> */}
+      <canvas id="canV" width="256" height="256" ref={canvasRef}></canvas>
+      <button onClick={openFile}>Press</button>
+    </div>
+  );
 }
+
+export default App;
+
 // function openFile() {
 //   dialog.showOpenDialog({ properties: ['openFile'] })
 //     .then((result) => {
@@ -20,32 +74,3 @@ const pressed = () => {
 //       console.error('Error while opening file dialog:', error);
 //     });
 // }
-const handleFileSelect = (event) => {
-    const file = event.target.files[0]; // Get the selected file
-  
-    if (file) {
-      const filePath = URL.createObjectURL(file); // Get a temporary URL for the selected file
-      console.log('Selected file path:', filePath);
-    }
-  
-}
-
-function openFile() {
-  ipcRenderer.invoke('open-file-dialog')
-    .then((filePath) => {
-      console.log('Selected file path:', filePath);
-    })
-    .catch((error) => {
-      console.error('Error while opening file dialog:', error);
-    });
-}
-  return (
-    <div className="App">
-      {/* <input type='file' onChange={handleFileSelect}/> */}
-      {/* <h1>VidKraft</h1> */}
-      <button onClick={pressed}>Press</button>
-    </div>
-  );
-}
-
-export default App;
--- src/middleware/middleware.js
+++ src/middleware/middleware.js
@@ -1,7 +1,8 @@
 const { app, BrowserWindow } = require('electron');
 const { ipcMain } = require("electron");
 const { dialog } = require('electron');
-
+const fs = require('fs');
+const path = require('path');
 const { convertVideo } = require('../backend/videoProcess')
 
 const inputFile = 'D:\\Projects\\video-editor\\TestVideos\\cycles.mkv';
@@ -24,13 +25,21 @@ const outputFile = 'D:\\Projects\\video-editor\\TestVideos\\cycles.mp4';
 });
 
 ipcMain.handle('open-file-dialog', async () => {
+  const publicFolderPath = path.join(__dirname, '/test.mp4');
+
     const mainWindow = BrowserWindow.getFocusedWindow();
   
     const result = await dialog.showOpenDialog(mainWindow, { properties: ['openFile'] });
   
     if (!result.canceled && result.filePaths.length > 0) {
-      return result.filePaths[0];
-    }
-  
+     fs.copyFile(result.filePaths[0], publicFolderPath, (error) => {
+      if (error) {
+        console.error('Error copying file:', error);
+      } else {
+        console.log('File copied successfully!');
+      }
+    });
+    return result.filePaths[0];
+  }
     return null;
   });
